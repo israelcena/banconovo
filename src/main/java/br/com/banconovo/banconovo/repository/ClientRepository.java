2fc7545fb5f8ce635338af1e8487664b7d3c4fe6
israelcena <israelsjm@gmail.com>
2022-02-27 07:33:10 -0300

    create crud Repository

---
+++ src/main/java/br/com/banconovo/banconovo/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package br.com.banconovo.banconovo.repository;
+
+import br.com.banconovo.banconovo.model.DefaultClient;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+@Repository
+public interface ClientRepository extends CrudRepository<DefaultClient, UUID> {
+  List<DefaultClient> findByName(String name);
+}
