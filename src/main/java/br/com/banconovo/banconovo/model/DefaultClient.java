9dab0a0565723308da196a3e7e0d1755d353f384
israelcena <israelsjm@gmail.com>
2022-02-27 06:24:00 -0300

    update client name

--- src/main/java/br/com/banconovo/banconovo/model/DefaultClient.java
+++ src/main/java/br/com/banconovo/banconovo/model/DefaultClient.java
@@ -4,7 +4,9 @@ import lombok.Data;
 import java.util.UUID;
 
 @Data
-public class Client {
+public class DefaultClient {
   private UUID id;
   private String name;
+  private String email;
+  private String password;
 }
