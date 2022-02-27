36167c82f4568505d2ce9a1855f8b362bf5abbc9
israelcena <israelsjm@gmail.com>
2022-02-27 07:33:39 -0300

    generate auto id

--- src/main/java/br/com/banconovo/banconovo/model/DefaultClient.java
+++ src/main/java/br/com/banconovo/banconovo/model/DefaultClient.java
@@ -1,10 +1,16 @@
 package br.com.banconovo.banconovo.model;
 
 import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.UUID;
 
-@Data
+@Data@Entity
 public class DefaultClient {
+  @Id  @GeneratedValue(strategy=GenerationType.AUTO)
   private UUID id;
   private String name;
   private String email;
