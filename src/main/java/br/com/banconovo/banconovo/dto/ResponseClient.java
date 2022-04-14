2e12fbeb8da2774cd89ede81bf220d29eba2e872
Israel Cena <israelsjm@gmail.com>
2022-04-14 14:44:45 -0300

    remove @Data anotation

--- src/main/java/br/com/banconovo/banconovo/dto/ResponseClient.java
+++ src/main/java/br/com/banconovo/banconovo/dto/ResponseClient.java
@@ -1,27 +1,27 @@
 package br.com.banconovo.banconovo.dto;
 
 import br.com.banconovo.banconovo.model.DefaultClient;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-@Data
+@Getter
+@Setter
 public class ResponseClient {
-  private UUID id;
+//  private Long id;
   private String name;
   private String email;
 
   public ResponseClient(DefaultClient defaultClient){
-    this.id = defaultClient.getId();
+//    this.id = defaultClient.getId();
     this.name = defaultClient.getName();
     this.email = defaultClient.getEmail();
   }
 
   public static List<ResponseClient> toResponse(Iterable<DefaultClient> defaultClients){
-//    defaultClients.stream().map(ResponseClient::new).collect(Collectors.toList());
     return StreamSupport.stream(defaultClients.spliterator(), false).map(ResponseClient::new).collect(Collectors.toList());
   }
 }
