6ad74c1cfeab504f58cc662952ecbc1370f8480c
Israel Cena <israelsjm@gmail.com>
2022-04-14 14:46:34 -0300

    rework ClientController.java

--- src/main/java/br/com/banconovo/banconovo/controller/ClientController.java
+++ src/main/java/br/com/banconovo/banconovo/controller/ClientController.java
@@ -2,42 +2,44 @@ package br.com.banconovo.banconovo.controller;
 
 import br.com.banconovo.banconovo.dto.ResponseClient;
 import br.com.banconovo.banconovo.model.DefaultClient;
-import br.com.banconovo.banconovo.repository.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.banconovo.banconovo.service.ClientService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.UUID;
+import java.util.List;
 
-@RestController@RequestMapping("/clientes")
+@RestController
+@RequestMapping("/clientes")
+@RequiredArgsConstructor
 public class ClientController {
 
-  @Autowired
-  private ClientRepository clientRepository;
+  private final ClientService clientService;
 
   @GetMapping
-  public Iterable<ResponseClient> listClients(){
-    return ResponseClient.toResponse(clientRepository.findAll());
+  public ResponseEntity<List<ResponseClient>> listAllClients(){
+    Iterable<DefaultClient> clients = clientService.findAll();
+    return ResponseEntity.ok(ResponseClient.toResponse(clients));
   }
 
   @PostMapping@ResponseStatus(HttpStatus.CREATED)
   public ResponseEntity<ResponseClient> handlerCreateClient(@RequestBody DefaultClient defaultClient, UriComponentsBuilder uriComponentsBuilder){
-    clientRepository.save(defaultClient);
+    clientService.saveOne(defaultClient);
     URI uri = uriComponentsBuilder.path("/clientes/{id}").buildAndExpand(defaultClient.getId()).toUri();
     return ResponseEntity.created(uri).body(new ResponseClient(defaultClient));
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<ResponseClient> detailClient(@PathVariable UUID id) throws Exception {
-    return ResponseEntity.ok(new ResponseClient(clientRepository.findClientById(id)));
+  public ResponseEntity<ResponseClient> detailClient(@PathVariable Long id) {
+    return ResponseEntity.ok(new ResponseClient(clientService.findClientById(id)));
   }
 
   @DeleteMapping("/{id}")@ResponseStatus(HttpStatus.OK)
-  public ResponseEntity deleteThisClient (@PathVariable UUID id) throws Exception{
-    clientRepository.deleteById(id);
+  public ResponseEntity deleteThisClient (@PathVariable Long id) {
+    clientService.deleteClient(id);
     return ResponseEntity.ok().build();
   }
 }
